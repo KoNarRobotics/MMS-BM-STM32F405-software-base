ea30bc76d700bfe1bba2fcf72263ca431ed0516c
Patryk Dudizński <dodpat02@gmail.com>
2025-10-27 16:12:04 +0100

    added defalut injitializaions

--- src/module.cpp
+++ src/module.cpp
@@ -1,6 +1,5 @@
 
 #include "module.hpp"
-#include "MT6701.hpp"
 #include "i2c.hpp"
 #include "main.hpp"
 #include "main_prog.hpp"
@@ -8,6 +7,13 @@
 #include "stmepic.hpp"
 
 se::Status init_board() {
+  STMEPIC_ASSING_TO_OR_RETURN(
+      i2c,
+      se::I2C::Make(hi2c2, pin_i2c_sda, pin_i2c_scl, se::HardwareType::DMA));
+
+  STMEPIC_ASSING_TO_OR_RETURN(uart,
+                              se::UART::Make(huart4, se::HardwareType::DMA));
+
   ////////////////////////////////////////////////////////////////////////////
   /// PLACE FOR YOUR CUSTOM MODULES INITIALIZATION
 
